efab144e8e8ec37a2e184d2a9638017cdb76bf9a
Shaonkabir <shaonkabir98@gmail.com>
2020-04-03 11:23:25 +0600

    refactor(Steps): successfully add tour steps

--- src/Steps.js
+++ src/Steps.js
@@ -1,6 +1,9 @@
 const steps = [
   {
-    attachTo: ".logo__img bottom",
+    attachTo: {
+      element: ".logo__img",
+      on: "bottom"
+    },
     buttons: [
       {
         classes: "shepherd-button-primary",
@@ -13,10 +16,8 @@ const steps = [
         type: "next"
       }
     ],
-    title: "lsdjflasjflasfjdldsjfldaj",
-    text: [
-      "React-Shepherd is a JavaScript library for guiding users through your React app."
-    ],
+    title: "Our coolest Logo",
+    text: ["This is our logo for React-Shepherd. It's amazing. isn't it?"],
     when: {
       show: () => {
         console.log("show step");
@@ -28,14 +29,11 @@ const steps = [
   },
 
   {
-    attachTo: ".nav__link bottom",
-
+    attachTo: {
+      element: ".nav__link",
+      on: "bottom"
+    },
     buttons: [
-      {
-        classes: "shepherd-button-secondary",
-        text: "Exit",
-        type: "cancel"
-      },
       {
         classes: "shepherd-button-primary",
         text: "Back",
@@ -47,10 +45,8 @@ const steps = [
         type: "next"
       }
     ],
-    title: "Welcome to React-Shepherd!",
-    text: [
-      "React-Shepherd is a JavaScript library for guiding users through your React app."
-    ],
+    title: "Navigation Section",
+    text: ["It's our cool navigation build with css flexbox."],
     when: {
       show: () => {
         console.log("show step");
@@ -59,7 +55,120 @@ const steps = [
         console.log("hide step");
       }
     }
-  }
+  },
+  {
+    attachTo: {
+      element: ".hero__text",
+      on: "top"
+    },
+    buttons: [
+      {
+        classes: "shepherd-button-primary",
+        text: "Back",
+        type: "back"
+      },
+      {
+        classes: "shepherd-button-primary",
+        text: "Next",
+        type: "next"
+      }
+    ],
+    title: "Hero Title",
+    text: ["This is our main Slogan to go ahead."],
+    when: {
+      show: () => {
+        console.log("show step");
+      },
+      hide: () => {
+        console.log("hide step");
+      }
+    }
+  },
+  {
+    attachTo: {
+      element: ".boxed-btn",
+      on: "left"
+    },
+    buttons: [
+      {
+        classes: "shepherd-button-primary",
+        text: "Back",
+        type: "back"
+      },
+      {
+        classes: "shepherd-button-primary",
+        text: "Next",
+        type: "next"
+      }
+    ],
+    title: "About Us!",
+    text: ["Click here and learn more about us."],
+    when: {
+      show: () => {
+        console.log("show step");
+      },
+      hide: () => {
+        console.log("hide step");
+      }
+    }
+  },
+  {
+    attachTo: {
+      element: ".dummy .dummy__left .dummy__heading",
+      on: "right"
+    },
+    buttons: [
+      {
+        classes: "shepherd-button-primary",
+        text: "Back",
+        type: "back"
+      },
+      {
+        classes: "shepherd-button-primary",
+        text: "Next",
+        type: "next"
+      }
+    ],
+    title: "It's our services one",
+    text: ["It's our awesome services one."],
+    when: {
+      show: () => {
+        console.log("show step");
+      },
+      hide: () => {
+        console.log("hide step");
+      }
+    }
+  },
+  {
+    attachTo: {
+      element: ".dummy .dummy__left .bordered-btn",
+      on: "bottom"
+    },
+    buttons: [
+      {
+        classes: "shepherd-button-primary",
+        text: "Back",
+        type: "back"
+      },
+      {
+        classes: "shepherd-button-primary",
+        text: "Done !",
+        type: "next"
+      }
+    ],
+    title: "Explore more!",
+    text: ["Click here to explore more about our services"],
+    when: {
+      show: () => {
+        console.log("show step");
+      },
+      hide: () => {
+        console.log("hide step");
+      }
+    }
+  },
+  
 ];
 
 export default steps;
